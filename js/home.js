8e131edc96da471939dcf6d84aa658df7d9309a5
Louis <louis.morauxupgrade@gmail.com>
2024-04-02 19:36:31 +0200

    changement total DA4

---
+++ js/home.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const menuList = document.querySelector(".menu-list");
+  
+    menuToggle.addEventListener("click", function() {
+      menuList.classList.toggle("active");
+    });
+  });
+  
